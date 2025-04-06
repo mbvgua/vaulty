8369c281b937be92acca85e36e3ba2783e10147d
mbvgua <mbuguadouglas17@gmail.com>
2025-04-06 11:21:57 +0300

    Created the first endpoint!!(technically 2nd)
    Created the register user endpoint in the rather simple way with no
    validation or authorization.
    
    Had to rewrite the database schema, in that the initial INT data type
    associated with the id column had to truncate the long uuid generated
    id. Converted this to VARCHAR(255) to house it without truncating it.
    
    build the routes and collections to help in eventual modularization of
    the application.

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -2,29 +2,17 @@ import mysql from 'mysql2/promise'
 import dotenv from 'dotenv'
 import path from 'path'
 
-dotenv.config({path:path.resolve(__dirname,'../../.env')})
+import { sqlConfiguration } from '../models'
+
 // dotenv.config() --DOES NOT WORK?!
+dotenv.config({path:path.resolve(__dirname,'../../.env')})
 // console.log(process.env.DB_USERNAME)
 
 
-
-async function dbConnection(){
-    const connection = await mysql.createConnection({
-        host:'localhost',
-        user:process.env.DB_USERNAME,
-        password:process.env.DB_PASSWORD,
-        database:process.env.DB_NAME    
-    })
-
-    try {
-        const [results,fields] = await connection.query(
-          'SELECT * FROM userBasicInfo;'
-        )  
-        console.log(results)
-        // console.log(fields) -RETURNS ENTIRE TABLE SCHEMA
-      } catch (error) {
-          console.log(error)
-      }
+// sql config values
+export const sqlConfig:sqlConfiguration = {
+    host:'localhost',
+    user:process.env.DB_USERNAME,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME
 }
-
-dbConnection()
