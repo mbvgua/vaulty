1dc1ea1618a3360ec1614605fc24adfd51530f48
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-04-22 16:24:10 +0300

    Error still persists even after declaring return type
    Experimenting with the registerUser function, where I decalred the
    return type to be a Promise<Response>, nothing changes, still getting
    the error, no overload matches this blah blah...

--- backend/src/api-v1/routes/users.routes.ts
+++ backend/src/api-v1/routes/users.routes.ts
@@ -3,16 +3,16 @@ import { addUserDetails, deactivateAccount, loginUser, reactivateAccount, regist
 import { getUsers, getUserByEmail, updateUser } from "../controllers/user-details.controllers"
 
 
-const authRouter:Router = Router()
+const authRouter = Router()
 
 authRouter.post('/register',registerUser)
-authRouter.post('/login',loginUser)
-authRouter.post('/add-details/:id',addUserDetails)
-authRouter.get('/get-all',getUsers)
-authRouter.get('/get-by-email',getUserByEmail)
-authRouter.patch('/update/:id',updateUser)
-authRouter.put('/deactivate/:id',deactivateAccount)
-authRouter.patch('/reactivate/:id',reactivateAccount)
+// authRouter.post('/login',loginUser)
+// authRouter.post('/add-details/:id',addUserDetails)
+// authRouter.get('/get-all',getUsers)
+// authRouter.get('/get-by-email',getUserByEmail)
+// authRouter.patch('/update/:id',updateUser)
+// authRouter.put('/deactivate/:id',deactivateAccount)
+// authRouter.patch('/reactivate/:id',reactivateAccount)
 
 
 export default authRouter
