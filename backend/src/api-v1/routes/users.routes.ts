edf2d23e498a0b47d3a9f069a4c99b3fce36abc3
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-05-01 10:19:46 +0300

    Allow only admins to delete user accounts
    Incorporate functionality for admins to delete user accounts, This will
    be either using the users email or username which are unique for each
    user. If the user is not an admin, appropriate error messages will be
    returned.
    
    While doing this part, realises there are massive chunks of code that
    are quite redundant, say the part for checking whether an input is an
    email or username. Irregardless of either, the steps followed for each
    are massively similar, look into alternative ways to make this shorter
    somehow to reduce copy pasting.

--- backend/src/api-v1/routes/users.routes.ts
+++ backend/src/api-v1/routes/users.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import { addUserDetails, deactivateAccount, loginUser, registerUser } from "../controllers/users.controllers"
-import { getUsers, getUserByEmail, updateUser } from "../controllers/user-details.controllers"
+import { getUsers, getUserByEmail, updateUser, deleteAccount } from "../controllers/user-details.controllers"
 
 
 const authRouter:Router = Router()
@@ -12,6 +12,7 @@ authRouter.get('/:id/get-all',getUsers)
 authRouter.get('/:id/get-by-email',getUserByEmail)
 authRouter.patch('/update/:id',updateUser)
 authRouter.put('/deactivate/:id',deactivateAccount)
+authRouter.delete('/:id/delete/',deleteAccount)
 
 
 export default authRouter
