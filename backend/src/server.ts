6dce52dc01b62bc006b5c59271651ea395b55c6a
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-06-26 18:23:42 +0300

    feat(backend/services): send email verification
    after user succesfully registers into the system, they wil receive an
    email requesting them to verify their account shortly after. This will
    help to ensure security and maybe also prevent bots from registering.
    
    The request to verify their email will be send at diff intervals from
    the congratulatory success message.
    
    TODO: maybe delete user accounts that have not successfuly verified
    their emails after a given time? probably two weeks.
    TODO: add a helper to the service, that renders the template

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,21 @@
 import app from "./app";
 import dotenv from "dotenv";
 import cron from "node-cron";
-import { welcomeUser } from "./api-v1/services/welcome-email.service";
+import { sendWelcomeEmail } from "./api-v1/services/welcome-email.service";
+import { sendVerificationEmail } from "./api-v1/services/verify-email.service";
 
 dotenv.config();
 
 const port = process.env.PORT;
 
-cron.schedule("*/1 * * * *", () => {
-  console.log("running task every 10 seconds");
-    welcomeUser()
+cron.schedule("*/30 * * * * *", async() => {
+  console.log("running task every 30 secs");
+  await sendWelcomeEmail();
+});
+
+cron.schedule("*/1 * * * *", async() => {
+  console.log("running task every minute");
+  await sendVerificationEmail();
 });
 
 app.listen(port, () => {
