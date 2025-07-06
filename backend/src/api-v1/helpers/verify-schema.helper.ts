9fe7f18e1b9a4c21d9ab485ce4e026741d536bc0
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-07-06 07:38:44 +0300

    feat(logging): got the logs working!!
    while attending the angular dev fest, i thought of a hack to amke things
    work and it worked. Removed logging of registration/login attempts
    and only logged it if succesful registered from the db or unsuccesful in
    teh validation helper. The error which previously broke the
    request/response cycle was eradicated and everything now worked
    effortlessly.
    
    WIN: debugging skills always on top!!

--- backend/src/api-v1/helpers/verify-schema.helper.ts
+++ backend/src/api-v1/helpers/verify-schema.helper.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { logger } from "../../config/winston.config";
 
 export async function validationHelper(
   request: Request,
@@ -8,6 +9,11 @@ export async function validationHelper(
   const { error } = schema.validate(request.body);
 
   if (error) {
+    logger.error({
+      message: "User validation error",
+      data: error.details[0].message,
+    });
+
     return response.status(422).json({
       code: 422,
       status: "error",
