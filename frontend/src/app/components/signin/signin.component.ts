5ac308743d12b605d93f85d89b988952a009a2df
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-07-15 23:12:06 +0300

    style:lipstick:(frontend):add auth main components
    - add the main auth components to allow loggin in to the system. These
      are:
        - signin
        - signup
        - navbar and footer
        - main homepage
    
    - Currently these are also the only endpoints I have in the backend, and
      hence will allow integration with it.
    
    WINS: effectively used bootstrap using their docs

--- frontend/src/app/components/signin/signin.component.ts
+++ frontend/src/app/components/signin/signin.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.scss'
 })
